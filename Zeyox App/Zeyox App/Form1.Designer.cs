2ac4f8d76fffc2c4af68101cdc811e521454ecdb
Yusuf <=>
2024-10-12 12:20:41 +0300

    Proje Oluşturuldu Ve İskelet oluşturuldu

---
+++ Zeyox App/Zeyox App/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace Zeyox_App
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.addressbutton = new System.Windows.Forms.Button();
+            this.Login_Registerbutton = new System.Windows.Forms.Button();
+            this.BasketButton = new System.Windows.Forms.Button();
+            this.FavoriteButton = new System.Windows.Forms.Button();
+            this.searchbar = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.addressbutton);
+            this.panel1.Controls.Add(this.Login_Registerbutton);
+            this.panel1.Controls.Add(this.BasketButton);
+            this.panel1.Controls.Add(this.FavoriteButton);
+            this.panel1.Controls.Add(this.searchbar);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1231, 76);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(774, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ürün Ara";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1158, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Adres Bilgileri";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1048, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Giriş yap/Kaydol";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(113, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sepetim";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Favoriler";
+            // 
+            // addressbutton
+            // 
+            this.addressbutton.Image = ((System.Drawing.Image)(resources.GetObject("addressbutton.Image")));
+            this.addressbutton.Location = new System.Drawing.Point(1168, 4);
+            this.addressbutton.Name = "addressbutton";
+            this.addressbutton.Size = new System.Drawing.Size(50, 50);
+            this.addressbutton.TabIndex = 1;
+            this.addressbutton.UseVisualStyleBackColor = true;
+            // 
+            // Login_Registerbutton
+            // 
+            this.Login_Registerbutton.AllowDrop = true;
+            this.Login_Registerbutton.Image = ((System.Drawing.Image)(resources.GetObject("Login_Registerbutton.Image")));
+            this.Login_Registerbutton.Location = new System.Drawing.Point(1063, 3);
+            this.Login_Registerbutton.Name = "Login_Registerbutton";
+            this.Login_Registerbutton.Size = new System.Drawing.Size(50, 50);
+            this.Login_Registerbutton.TabIndex = 1;
+            this.Login_Registerbutton.UseVisualStyleBackColor = true;
+            // 
+            // BasketButton
+            // 
+            this.BasketButton.Image = global::Zeyox_App.Properties.Resources.basket;
+            this.BasketButton.Location = new System.Drawing.Point(110, 3);
+            this.BasketButton.Name = "BasketButton";
+            this.BasketButton.Size = new System.Drawing.Size(50, 50);
+            this.BasketButton.TabIndex = 1;
+            this.BasketButton.UseVisualStyleBackColor = true;
+            // 
+            // FavoriteButton
+            // 
+            this.FavoriteButton.Image = ((System.Drawing.Image)(resources.GetObject("FavoriteButton.Image")));
+            this.FavoriteButton.Location = new System.Drawing.Point(3, 3);
+            this.FavoriteButton.Name = "FavoriteButton";
+            this.FavoriteButton.Size = new System.Drawing.Size(50, 50);
+            this.FavoriteButton.TabIndex = 1;
+            this.FavoriteButton.UseVisualStyleBackColor = true;
+            // 
+            // searchbar
+            // 
+            this.searchbar.Location = new System.Drawing.Point(295, 19);
+            this.searchbar.Multiline = true;
+            this.searchbar.Name = "searchbar";
+            this.searchbar.Size = new System.Drawing.Size(470, 34);
+            this.searchbar.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 110);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Kategoriler";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(327, 146);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 53);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Teknoloji";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(47, 146);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 53);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Erkek";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.Location = new System.Drawing.Point(786, 149);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(109, 50);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Oyun";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Image = ((System.Drawing.Image)(resources.GetObject("button5.Image")));
+            this.button5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button5.Location = new System.Drawing.Point(654, 146);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(123, 53);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Kozmatik";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button6.Image = ((System.Drawing.Image)(resources.GetObject("button6.Image")));
+            this.button6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button6.Location = new System.Drawing.Point(185, 146);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(101, 53);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Kadın";
+            this.button6.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button7.Image = ((System.Drawing.Image)(resources.GetObject("button7.Image")));
+            this.button7.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button7.Location = new System.Drawing.Point(503, 146);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(116, 53);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Kitap";
+            this.button7.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1255, 614);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Zeypx App";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox searchbar;
+        private System.Windows.Forms.Button FavoriteButton;
+        private System.Windows.Forms.Button BasketButton;
+        private System.Windows.Forms.Button Login_Registerbutton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button addressbutton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
